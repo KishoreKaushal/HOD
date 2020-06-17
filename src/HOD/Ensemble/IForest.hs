ae45d8690d7f1469496e22e3ef39f907a01d6962
Kaushal Kishore <kshr4kshl@gmail.com>
2020-06-17 17:50:19 +0530

    null node

--- src/HOD/Ensemble/IForest.hs
+++ src/HOD/Ensemble/IForest.hs
@@ -2,13 +2,14 @@ module HOD.Ensemble.IForest where
 
 import Data.List.Unique (uniq)
 
-data ITree = ITree {
-        right :: ITree,
-        left :: ITree,
-        splitAttr :: Int, 
-        splitVal :: Double, 
-        size :: Int
-    } deriving (Eq, Show)
+data ITree  = None 
+            | Node {
+                right :: ITree,
+                left :: ITree,
+                splitAttr :: Int, 
+                splitVal :: Double, 
+                size :: Int
+            } deriving (Eq, Show)
 
 data IForest = IForest {
         numTrees :: Int,
