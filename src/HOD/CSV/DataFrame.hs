8d776614e2d29831827740b9ff975e6f6dbee286
Kaushal Kishore <kshr4kshl@gmail.com>
2020-06-17 19:49:42 +0530

    function for parsing dataframe from csv

--- src/HOD/CSV/DataFrame.hs
+++ src/HOD/CSV/DataFrame.hs
@@ -1,4 +1,24 @@
 module HOD.CSV.DataFrame where
 
-import Text.CSV (parseCSVFromFile)
+import Text.CSV (parseCSVFromFile, CSV)
+import Text.Parsec.Error (ParseError)
 
+data DataFrame  = EmptyDataFrame
+                | DataFrame {
+                    header :: [String],
+                    colIdx :: [Int],
+                    rowIdx :: [Int],
+                    dat :: [[Double]]
+                } deriving(Show)
+
+
+getDataFrameFromCSV :: CSV -> DataFrame
+getDataFrameFromCSV csv' = EmptyDataFrame
+
+
+fromCSV :: FilePath -> IO (DataFrame)
+fromCSV fp = do 
+    parsedCSV <- parseCSVFromFile fp
+    case parsedCSV of 
+        Left err -> error "Error in parsing."
+        Right csv' -> return $ getDataFrameFromCSV csv'
