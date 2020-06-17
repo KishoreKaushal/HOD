3ace47ce71a0f15a3cad625aa20971f536ef42ce
Kaushal Kishore <kshr4kshl@gmail.com>
2020-06-17 17:46:38 +0530

    declaration of Iforest record

--- src/HOD/Ensemble/IForest.hs
+++ src/HOD/Ensemble/IForest.hs
@@ -1,5 +1,22 @@
 module HOD.Ensemble.IForest where
 
+import Data.List.Unique (uniq)
+
+data ITree = ITree {
+        right :: ITree,
+        left :: ITree,
+        splitAttr :: Int, 
+        splitVal :: Double, 
+        size :: Int
+    } deriving (Eq, Show)
+
+data IForest = IForest {
+        numTrees :: Int,
+        subsamplingSize :: Int, 
+        itrees :: [ITree]
+    } deriving (Eq, Show)
+
+
 sayHello :: IO ()
 sayHello = do
-    putStrLn "hello world"
+    putStrLn "hello from IForest"
